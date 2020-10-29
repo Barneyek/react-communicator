a1a2c4c1952e0cc10296fab2b8ec002d98c7b3d3
Damian Osiecki <osieckidamian1992@gmail.com>
2020-10-29 22:39:48 +0100

    Deploy gh pages

---
+++ build/precache-manifest.2e70231022f93386c53c5a2aa087d227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5307f216a0a513f2d417aa1198492342",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5ef1c1e349cd3dadd05f",
+    "url": "./static/css/main.dc240edb.chunk.css"
+  },
+  {
+    "revision": "97f9b9d8505aa80774c6",
+    "url": "./static/js/2.e8352967.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "./static/js/2.e8352967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef1c1e349cd3dadd05f",
+    "url": "./static/js/main.367b3330.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
